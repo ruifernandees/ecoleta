7d5e2c625c486c6c71e1666a372a876c34112b9c
ruifernandees <dev.ruifernandes@gmail.com>
2020-06-03 22:44:07 -0300

    Front-end web: redirect after post new point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -38,6 +38,8 @@ const CreatePoint = () => {
         email: '',
         whatsapp: ''
     });
+
+    const history = useHistory();
 
     const [selectedUf, setSelectedUf] = useState<string>('0');
     const [selectedCity, setSelectedCity] = useState<string>('0');
@@ -138,6 +140,7 @@ const CreatePoint = () => {
 
         await api.post('points', data);
         alert('Ponto de coleta criado!');
+        history.push('/');
     }
 
     return (
